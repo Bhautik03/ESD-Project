87f9e0c5d0785b33774187f096fbed817451fe9c
Bhautik03 <126908149+Bhautik03@users.noreply.github.com>
2025-11-28 12:56:19 +0530

    Update

--- src/main/java/com/example/demo/timetable/dto/StudentDto.java
+++ src/main/java/com/example/demo/timetable/dto/StudentDto.java
@@ -1,5 +1,12 @@
 package com.example.demo.timetable.dto;
 
 public record StudentDto(
-        Long id, String rollNumber, String firstName, String lastName, String email) {}
-
+                Long id,
+                String rollNumber,
+                String firstName,
+                String lastName,
+                String email,
+                Double cgpa,
+                Integer totalCredits,
+                Integer graduationYear) {
+}
